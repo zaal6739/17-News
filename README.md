4559540f954da7aef2ba4704e97d0916fd9cbdfc
Zack <zack1444@gmail.com>
2019-01-14 00:32:11 -0800

    redid activity with working post

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# 17-News
+APNews Scraper
--- server.js
+++ server.js
@@ -1,20 +1,53 @@
-const express = require('express');
-const app = express();
-const expbs = require('express-handlebars');
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Requiring our Note and Article models
 
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
-
-app.engine('handlebars',expbs({defaultLayout: 'main'}));
-app.set('view engine','handlebars');
-
-//routing
-const routes = require("./controllers/scraperController.js");
-
-app.use("/", routes);
+// Mongoose Promise
+mongoose.Promise = Promise;
 
 var PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log('Server is listening at port',PORT);
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Serve static content
+app.use(express.static("public"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/scraperController.js");
+
+app.use("/", routes);
+
+mongoose.connect("mongodb://admin:admin@ds215089.mlab.com:15089/heroku_6phm3dhh");
+
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connected");
+});
+
+// Listen on port 8080
+app.listen(PORT, function() {
+  console.log("App running on PORT " + PORT);
 });
